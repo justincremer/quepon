95a560737af0cf7437c8910f1ae17dfad3445fbb
jacremer <jacremer@live.com>
2021-08-09 22:39:19 -0700

    cleans out dead code + adds licenses

--- extension/src/index.tsx
+++ extension/src/index.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +32,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
